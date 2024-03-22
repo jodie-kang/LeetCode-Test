521f551e9421cc4abc48f76b1263519f76675686
kjq_glb <jkan0031@student.monash.edu>
2024-03-22 23:57:51 +0800

    add day3

---
+++ 2.两数相加.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=2 lang=python3
+#
+# [2] 两数相加
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if len(l1) == len(l2):
+            return l1 + l2
+# @lc code=end
+
